b34b8bd78c9bcdde01947f826d130a83feda54aa
sarthak0318 <singh0300sarthakk@gmail.com>
2021-07-23 16:05:08 +0530

    23/07 4:05

--- ecommsite/store/views.py
+++ ecommsite/store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
+
+def index(request):
+    item = product.objects.all()
+    return render(request, 'store/index.html', {'item':item})
