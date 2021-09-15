d411a90813ccacd0e15898c4853f0fd10083e5d9
sarthak0318 <singh0300sarthakk@gmail.com>
2021-09-15 20:42:52 +0530

    15-09 8:42

--- ecommsite/users/models.py
+++ ecommsite/users/models.py
@@ -53,7 +53,7 @@ class Account(AbstractBaseUser):
     is_superadmin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'first_name']
 
     objects = MyAccountManager()
 
--- ecommsite/users/urls.py
+++ ecommsite/users/urls.py
@@ -19,9 +19,15 @@ from . import views
 # from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name = 'register'),
     path('login/', views.login, name = 'login'),
     path('logout/', views.logout, name = 'logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('forgotpassword/', views.forgotpassword, name='forgotpassword'),
+    path('resetpassword/', views.resetpassword, name='resetpassword'),
 ]
 
 # urlpatterns = [
